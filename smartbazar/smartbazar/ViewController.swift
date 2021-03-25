cf3f309770d99677cf9e6b3bc098869f73e8a879
DoSSi4 <57913839+DoSSi4@users.noreply.github.com>
2021-03-25 13:00:57 +0600

    Add files via upload

--- smartbazar/smartbazar/ViewController.swift
+++ smartbazar/smartbazar/ViewController.swift
@@ -8,17 +8,31 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, UITabBarDelegate{
+class ViewController: UIViewController, UITabBarDelegate, WKNavigationDelegate{
     @IBOutlet var webview: WKWebView!
     @IBOutlet weak var tabBar: UITabBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         tabBar.delegate = self
+        webview.navigationDelegate = self
         webview.load(URLRequest(url: URL(string:"https://smartbazar.kz")!))
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(reloadWebView(_:)), for: .valueChanged)
            webview.scrollView.addSubview(refreshControl)
+        webview.allowsBackForwardNavigationGestures = true
+        webview.allowsLinkPreview = true
+        webview.sizeToFit()
+        
+    }
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let host = navigationAction.request.url?.host{
+            if host.contains("smartbazar.kz"){
+                decisionHandler(.allow)
+                return
+            }
+        }
+            decisionHandler(.cancel)
         
     }
     @objc func reloadWebView(_ sender: UIRefreshControl){
@@ -26,24 +40,26 @@ class ViewController: UIViewController, UITabBarDelegate{
         sender.endRefreshing()
     
     }
+        
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+    
         if(item.tag == 1){
             webview.load(URLRequest(url: URL(string: "https://smartbazar.kz")!))
         }
         else if (item.tag == 2){
-            webview.load(URLRequest(url: URL(string: "https://smartbazar.kz/products/75")!))
-            
+            webview.load(URLRequest(url: URL(string: "https://smartbazar.kz/catalog")!))
         }
         else if (item.tag == 3){
-            webview.load(URLRequest(url: URL(string: "https://smartbazar.kz/products/74")!))
+            webview.load(URLRequest(url: URL(string: "https://smartbazar.kz/wishlist")!))
         }
         else if (item.tag == 4){
-            webview.load(URLRequest(url: URL(string: "https://smartbazar.kz/products/92")!))
+            webview.load(URLRequest(url: URL(string: "https://smartbazar.kz/cart")!))
         }
         else if (item.tag == 5){
-            webview.load(URLRequest(url: URL(string: "https://smartbazar.kz/products/73")!))
+            webview.load(URLRequest(url: URL(string: "https://smartbazar.kz/profile")!))
         }
         }
+    
     
     }
 
